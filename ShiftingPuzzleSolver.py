b2b0668cfa6fc23d9bb5337ddf25d8321968a16c
DSSzymanski <dszymanski@umass.edu>
2020-02-10 16:21:46 -0500

    added rest of brute force, still needs adequate testing

--- ShiftingPuzzleSolver.py
+++ ShiftingPuzzleSolver.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb  8 21:02:26 2020
-
 @author: Daniel
 """
+from collections import deque
 
-#def brute_force():
+CORRECT_PUZZLE = [1,2,3,4,5,6,7,8,9]
+
+def brute_force(puzzle):
+	global CORRECT_PUZZLE
+	if puzzle == CORRECT_PUZZLE: return [] #basecase
+	move_dict = get_moves_dict()
+	queue = deque()
+	queue.append((puzzle, []))
+	
+	while True:
+		current = queue.popleft()
+		puzzle, moves = current[0], current[1]
+		for i in range(9):
+			for j in move_dict[i]:
+				new_moves = moves.copy()
+				new_moves.append((i,j))
+				new_puzzle = puzzle.copy()
+				new_puzzle = swap(new_puzzle, i, j)
+				if new_puzzle == CORRECT_PUZZLE: return new_moves
+				queue.append((new_puzzle, new_moves))
 
 def get_moves_dict():
 	"""
@@ -14,20 +33,23 @@ def get_moves_dict():
 	3---4---5
 	|   |   |
 	6---7---8
+	Moves where the first position moves to a lower position ie 8 -> 5 because
+	that swap is already accounted for
 	"""
 	moves = {}
 	moves[0] = [1,3]
-	moves[1] = [0,2,4]
-	moves[2] = [1,5]
-	moves[3] = [0,4,6]
-	moves[4] = [1,3,5,7]
-	moves[5] = [2,4,8]
-	moves[6] = [3,7]
-	moves[7] = [4,6,8]
-	moves[8] = [5,7]
+	moves[1] = [2,4]
+	moves[2] = [5]
+	moves[3] = [4,6]
+	moves[4] = [3,5,7]
+	moves[5] = [8]
+	moves[6] = [7]
+	moves[7] = [8]
+	moves[8] = []
 	
 	return moves
 	
 def swap(puzzle, tile_1, tile_2):
 	puzzle[tile_1], puzzle[tile_2] = puzzle[tile_2], puzzle[tile_1]
 	return puzzle
+
--- Tests.py
+++ Tests.py
@@ -7,7 +7,7 @@ Created on Sat Feb  8 21:06:26 2020
 
 import unittest
 import ShiftingPuzzleSolver as SPS
-
+5
 class TestSwap(unittest.TestCase):
 	test_puzzle = []
 	for i in range(5):
@@ -19,6 +19,7 @@ class TestSwap(unittest.TestCase):
 		key = [0, 1, 3, 2, 4]
 		self.assertEqual(test_puzzle, key)
 		
+	"""
 	def test_input_tiles(self):
 		valid_tile = 2
 		invalid_tile_1 = -1 #out of range
@@ -28,7 +29,23 @@ class TestSwap(unittest.TestCase):
 		for test in test_tiles:
 			self.assertEqual(SPS.swap(self.test_puzzle, test, valid_tile), None)
 			self.assertEqual(SPS.swap(self.test_puzzle, valid_tile, test), None)
-		
+	"""
+	
+class TestBruteForce(unittest.TestCase):
+	def test_puzzles(self):
+		#TP# = TestPuzzle(number), AP# = AnswerPuzzle(number)
+		TP1 = [1,2,3,4,5,6,7,8,9] #should return []
+		AP1 = []
+		TP2 = [2,1,3,4,5,6,7,8,9] #tests 1 swap in same row
+		AP2 = [(0,1)]
+		TP3 = [2,1,3,5,4,6,8,7,9] #tests 1 swap in row for all rows
+		AP3 = [(0,1),(3,4),(6,7)]
+		TP4 = [4,2,3,1,5,6,7,8,9] #tests horizontal swap
+		AP4 = [(0,3)]
+		tests = [TP1, TP2, TP3, TP4]
+		answers = [AP1, AP2, AP3, AP4]
+		for i in range(len(tests)):
+			self.assertEqual(SPS.brute_force(tests[i]), answers[i])	
 
 if __name__ == '__main__':
 	unittest.main()
