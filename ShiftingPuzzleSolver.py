09431e71e37040d8481bc48f09ca158a31ebb55e
DSSzymanski <dszymanski@umass.edu>
2020-02-23 22:02:10 -0500

    commented old code, started new code for GUI implementation. Added initial buttons and click functionality.

--- ShiftingPuzzleSolver.py
+++ ShiftingPuzzleSolver.py
@@ -6,6 +6,7 @@ Created on Sat Feb  8 21:02:26 2020
 from collections import deque
 from time import perf_counter
 from copy import deepcopy
+from tkinter import *
 import sys
 
 CORRECT_PUZZLE = [
@@ -13,30 +14,36 @@ CORRECT_PUZZLE = [
 	[3,4,5],
 	[6,7,8]]
 
-#works for basic puzzles, memory n!
+#puzzle solver based on heuristics
 def heuristic(puzzle):
 	global CORRECT_PUZZLE
 	
-	if puzzle == CORRECT_PUZZLE: return [] #basecase
+	#base case
+	if puzzle == CORRECT_PUZZLE: return []
 	
+	#initialize data structures
 	moves = []
 	queue = deque()
 	queue.append((puzzle, [], get_heuristic(puzzle)))
-	
 	states = set()
 	
-	#used to weed out bad moves
+	#used to weed out moves that result in a worst heuristic value
 	best_heuristic = get_heuristic(puzzle)
 	
 	while True:
 		current = queue.popleft()
 		puzzle, moves = current[0], current[1]
 		
-		#iterate over grid
+		#loop used to generate every move from every tile location
 		for i in range(3):
 			for j in range(3):
-				p_moves = [] #potential moves
-				#generate moves
+				#used to store potential moves for each tile
+				p_moves = [] 
+				
+				"""
+				Generates all moves from the tile location. Built in validation
+				to ensure all moves stay in the grid
+				"""
 				if i != 0: 
 					north = [i-1, j]
 					p_moves.append(north)
@@ -50,14 +57,22 @@ def heuristic(puzzle):
 					west = [i, j-1]
 					p_moves.append(west)
 				
+				
+				#Iterates over all valid moves generated for the tile
 				for move in p_moves:
-					#generate move, check if solved, if not add to queue
+					#create new puzzle & move lists & completes move
+					#and evaluates new heuristic
 					new_puzzle = deepcopy(puzzle)
 					new_moves = moves.copy()
-					
 					new_puzzle = swap(new_puzzle, [i, j], move)
 					new_heuristic = get_heuristic(new_puzzle)
 					
+					"""
+					Checks if heuristic is a good move and puzzle hasnt been
+					seen yet. If so, updates and checks if puzzle is solved. If
+					it is, returns, else adds to the queue and updates best
+					heuristic
+					"""
 					if new_heuristic <= best_heuristic and str(new_puzzle) not in states:
 						states.add(str(new_puzzle))
 						best_heuristic = new_heuristic
@@ -125,8 +140,8 @@ def get_tile_loc_dict():
 	return tiles
 
 """
-determines heuristic value of puzzle by adding up how far each tile is from 
-it's proper position
+determines heuristic value of puzzle by adding up how far each tile in the 
+puzzle is from it's proper position
 """
 def get_heuristic(puzzle):
 	tile_dic = get_tile_loc_dict()
@@ -143,12 +158,12 @@ def swap(puzzle, tile1, tile2):
 	puzzle[tile1[0]][tile1[1]], puzzle[tile2[0]][tile2[1]] = puzzle[tile2[0]][tile2[1]], puzzle[tile1[0]][tile1[1]]
 	return puzzle
 
-#prints puzzle
+#prints puzzle to console
 def puzzle_printer(puzzle):
 	for i in range(3):
 		print(puzzle[i])
 
-def main(argv):
+def console_solve(argv):
 	puzzle = []
 	row = []
 	for i in range(0, len(argv)):
@@ -171,5 +186,44 @@ def main(argv):
 		puzzle_printer(puzzle)
 	print(f"Steps: {len(ans)}\n Time: {end_time - start_time}")
 	
+#change button text to tiles
+def button_increment(button):
+	num = int(button.cget('text'))
+	num += 1
+	if num > 8: num = 0
+	button.config(text=num)
+	
+def gui_solve():
+	#initialize GUI
+	window = Tk()
+	window.title("Shifting Puzzle Solver")
+	
+	#set up and initialize all tile buttons to 0
+	tile_0_0 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_0_0))
+	tile_0_1 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_0_1))
+	tile_0_2 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_0_2))
+	tile_1_0 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_1_0))
+	tile_1_1 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_1_1))
+	tile_1_2 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_1_2))
+	tile_2_0 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_2_0))
+	tile_2_1 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_2_1))
+	tile_2_2 = Button(window, text="0", padx=40, pady=20, command=lambda: button_increment(tile_2_2))
+	
+	#show all tiles
+	tile_0_0.grid(row=0, column=0)
+	tile_0_1.grid(row=0, column=1)
+	tile_0_2.grid(row=0, column=2)
+	tile_1_0.grid(row=1, column=0)
+	tile_1_1.grid(row=1, column=1)
+	tile_1_2.grid(row=1, column=2)
+	tile_2_0.grid(row=2, column=0)
+	tile_2_1.grid(row=2, column=1)
+	tile_2_2.grid(row=2, column=2)
+	
+	#run
+	window.mainloop()
+
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	if len(sys.argv) == 1: gui_solve()
+	else: console_solve(sys.argv[2:])
+	
