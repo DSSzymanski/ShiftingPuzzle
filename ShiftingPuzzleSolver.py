78adeb1160e3eb6b41415a696a6ad042f96df8dd
DSSzymanski <dszymanski@umass.edu>
2020-05-21 19:21:40 -0400

    changed to class format for the GUId

--- ShiftingPuzzleSolver.py
+++ ShiftingPuzzleSolver.py
@@ -3,66 +3,88 @@
 Created on Sat Feb  8 21:02:26 2020
 @author: Daniel
 """
-from tkinter import *
-from tkinter import messagebox
 import Console
 from Puzzle import validate
 import sys
+import tkinter
+from tkinter import messagebox
 
-ERROR_MSG = "Tiles must represent 0-8, not repeating. Tiles 0-2 represents the \
-	top row, 3-5 the middle, and 6-8 the bottom row."
+class ShiftingPuzzleGUI(tkinter.Tk):
+	def __init__(self):
+		tkinter.Tk.__init__(self)
+		self._frame = None
+		self.switch_frame(PuzzleFrame)
+	
+	def switch_frame(self, frame_class):
+		new_frame = frame_class(self)
+		if self._frame is not None: 
+			self._frame.destroy()
+		self._frame = new_frame
+		self._frame.pack()
+		
+class PuzzleFrame(tkinter.Frame):
+	def __init__(self, master):
+		tkinter.Frame.__init__(self, master)
+		self.tiles_list = []
+		self._init_tiles()
+		self.solve_btn = tkinter.Button(self, text="Solve", command=lambda: self.solve())
+		self.solve_btn.grid(row=3, column=1)
+		self.ERROR_MSG = "Tiles must represent 0-8, not repeating. Tiles 0-2 represents the top row, 3-5 the middle, and 6-8 the bottom row."
+		
+	def _init_tiles(self):
+		self.tile_0_0 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_0_0))
+		self.tile_0_1 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_0_1))
+		self.tile_0_2 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_0_2))
+		self.tile_1_0 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_1_0))
+		self.tile_1_1 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_1_1))
+		self.tile_1_2 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_1_2))
+		self.tile_2_0 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_2_0))
+		self.tile_2_1 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_2_1))
+		self.tile_2_2 = tkinter.Button(self, text=0, padx=40, pady=20, command=lambda: self.button_increment(self.tile_2_2))
 
-#change button text to tiles
-def button_increment(button):
-	num = button.cget('text')
-	num += 1
-	if num > 8: num = 0
-	button.config(text=num)
+		self.tiles_list = [self.tile_0_0, self.tile_0_1, self.tile_0_2, \
+					 self.tile_1_0, self.tile_1_1, self.tile_1_2, self.tile_2_0, \
+					 self.tile_2_1, self.tile_2_2]
+		#show all tiles
+		self.tile_0_0.grid(row=0, column=0)
+		self.tile_0_1.grid(row=0, column=1)
+		self.tile_0_2.grid(row=0, column=2)
+		self.tile_1_0.grid(row=1, column=0)
+		self.tile_1_1.grid(row=1, column=1)
+		self.tile_1_2.grid(row=1, column=2)
+		self.tile_2_0.grid(row=2, column=0)
+		self.tile_2_1.grid(row=2, column=1)
+		self.tile_2_2.grid(row=2, column=2)
+		
+	#change button text to tiles
+	def button_increment(self, button):
+		num = button.cget('text')
+		num += 1
+		if num > 8: num = 0
+		button.config(text=num)
 	
-def get_tile_text(tiles):
-	return [tile.cget("text") for tile in tiles]
-
-def solve(tiles):
-	puzzle = get_tile_text(tiles)
-	if validate(puzzle):
-		return True
-	messagebox.showerror(title="Input error", message=ERROR_MSG)
+	"""
+	Checks to see if tiles give a valid puzzle.
+	If so, converts to puzzle and solves, changing to Solution Frame
+	If not, gives an error popup and stays on curr frame
+	"""
+	def solve(self):
+		puzzle = self.get_tiles()
+		if validate(puzzle):
+			return True
+		tkinter.messagebox.showerror(title="Input error", message=self.ERROR_MSG)
 	
-def gui_solve():
-	#initialize GUI
-	window = Tk()
-	window.title("Shifting Puzzle Solver")
-	
-	f1 = Frame(width=400, height=400)
-	#set up and initialize all tile buttons to 0
-	tile_0_0 = Button(f1, text=0, padx=40, pady=20, command=lambda: button_increment(tile_0_0))
-	tile_0_1 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_0_1))
-	tile_0_2 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_0_2))
-	tile_1_0 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_1_0))
-	tile_1_1 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_1_1))
-	tile_1_2 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_1_2))
-	tile_2_0 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_2_0))
-	tile_2_1 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_2_1))
-	tile_2_2 = Button(window, text=0, padx=40, pady=20, command=lambda: button_increment(tile_2_2))
-	tiles_list = [tile_0_0, tile_0_1, tile_0_2, tile_1_0, tile_1_1, tile_1_2, tile_2_0, tile_2_1, tile_2_2]
-   
-	tile_solve = Button(window, text="Solve", command=lambda: solve(tiles_list))
-	
-	#show all tiles
-	tile_0_0.grid(row=0, column=0)
-	tile_0_1.grid(row=0, column=1)
-	tile_0_2.grid(row=0, column=2)
-	tile_1_0.grid(row=1, column=0)
-	tile_1_1.grid(row=1, column=1)
-	tile_1_2.grid(row=1, column=2)
-	tile_2_0.grid(row=2, column=0)
-	tile_2_1.grid(row=2, column=1)
-	tile_2_2.grid(row=2, column=2)
-	tile_solve.grid(row=3, column=1)
-	
-	#run
-	window.mainloop()
+	#gets list of puzzle tiles
+	def get_tiles(self):
+		return [tile.cget("text") for tile in self.tiles_list]
+		
+class SolveFrame(tkinter.Frame):
+	def __init__(self, master):
+		tkinter.Frame.__init__(self, master)
+		tkinter.Button(self, text="Go to puzzle", command=lambda: master.switch_frame(PuzzleFrame)).pack()
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1: gui_solve()
+	if len(sys.argv) == 1: 
+		gui = ShiftingPuzzleGUI()
+		gui.mainloop()
 	else: Console.console_solve(sys.argv[1:])
