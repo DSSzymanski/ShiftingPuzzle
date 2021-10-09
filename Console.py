39b307b880394c2ad7b6fda9e04adc926c7959e3
dsszymanski <dan1993us@yahoo.com>
2021-10-09 05:43:13 -0400

    Finished the input validation and added the print statements for the console version. Finished documenting.

--- Console.py
+++ Console.py
@@ -1,27 +1,72 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat May 16 19:00:22 2020
+The console module is used to start the program as a command line program. Once
+started, the program validates the input and either returns an error or runs the
+program. If the input is ran, it will then print every state in the solution set
+to the console along with time information.
 
-@author: Daniel
+Methods
+-------
+console_solve(list argv) -> str:
+    validates input from command line, returns error if invalid input, runs program
+    and prints to console.
+
+Globals
+-------
+INVALID_INPUT_ERROR : str
+    error to print to console if the input isn't 0-8 non-repeating.
+TOO_MANY_INPUT_ERROR : str
+    error to print if more than 1 input is given.
+COMPLETION : str
+    string to return if the program is successfully ran.
 """
+
 from time import perf_counter
-import Heuristic as H
-from Puzzle import puzzle_printer, swap, puzzle_converter
+import heuristic as H
+from puzzle import Puzzle
 
+INVALID_INPUT_ERROR = "Invalid Input: input must be the numbers 0-8 non-repeating."
+TOO_MANY_INPUT_ERROR = "Too many inputs detected. Program needs 1 string, but got "
+COMPLETION = "Program completed."
 def console_solve(argv):
-	puzzle = puzzle_converter(argv)
+    """
+    console_solve is used to run the program in console form. The function takes
+    the input and validates it, returning either an error message for too many
+    inputs if there are more than 1 input in the list or an incorrect input if
+    the input isn't 0-8 non-repeating. If the input was validated, the program
+    creates a puzzle object and starts a timer. The program solves the puzzle
+    then stops the timer. Once stopped, the solutions set will be printed to
+    console with the final time, and finally returning the completion string.
 
-	start_time = perf_counter()
-	ans = H.heuristic(puzzle)
-	end_time = perf_counter()
-	
-	#printing answer
-	puzzle_printer(puzzle)
-	for a in ans:
-		swap(puzzle, a[0], a[1])
-		print("\n" + "Tile: " + str(puzzle[a[0][0]][a[0][1]]) + " -> " +\
-		   str(puzzle[a[1][0]][a[1][1]]))
-		print("Location: ", a[0], "->", a[1], "\n")
-		print("State:")
-		puzzle_printer(puzzle)
-	print(f"\nSteps: {len(ans)}\nTime: {end_time - start_time}")
+    Parameters
+    ----------
+    argv : list
+        list of inputs given through command prompt.
+
+    Returns
+    -------
+    str
+        Returns string representing error msg or completion msg.
+
+    """
+    if len(argv) > 1:
+        return TOO_MANY_INPUT_ERROR + f"{len(argv)} inputs."
+    puzzle_str = [int(i) for i in list(argv[0])]
+    if not Puzzle.validate(puzzle_str):
+        return print(INVALID_INPUT_ERROR)
+    puzzle = Puzzle(puzzle_str)
+
+    start_time = perf_counter()
+    puzzle = H.heuristic(puzzle)
+    soln_set = puzzle.get_soln_states()
+    end_time = perf_counter()
+
+    for idx, puzzle in enumerate(soln_set):
+        if idx == 0:
+            print("Initital State:")
+        else:
+            print(f"State: {idx}")
+        Puzzle.puzzle_printer(puzzle)
+        print()
+
+    print(f"Total time: {round(end_time - start_time, 5)}s.")
+    return COMPLETION
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import console
+"""
+Main module to be run to start the program. If ran with 0 inputs from the command
+line or ran as a program, it will show the GUI representation.
+
+If ran with more than 1 input will run the command line form of the program.
+This 1 input needs to be an input of 0-8 non-repeating.
+
+    An input of 012345678 or 81427653 will work as they have all the numbers from
+    zero to eight, while 012345670 or 81234543 will not as they have duplicate
+    numbers.
+"""
+
 import sys
+import console
 from puzzle_gui import ShiftingPuzzleGUI
 
 if __name__ == "__main__":
@@ -9,4 +21,6 @@ if __name__ == "__main__":
         gui.eval('tk::PlaceWindow . center')
         gui.mainloop()
     else:
-        console.console_solve(sys.argv[1:])
+        error = console.console_solve(sys.argv[1:])
+        if error != console.COMPLETION:
+            print(error)
